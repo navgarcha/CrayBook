0b31fcbff48d42323e91d5a3129d00449f1d174c
Navkaran Garcha <nav_garcha@hotmail.co.uk>
2012-05-05 15:28:45 +0000

    shitcray converted to use CoffeeScript

--- craybook.js
+++ craybook.js
@@ -1,66 +1,49 @@
+"use strict";
+var ShitCray;
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+ShitCray = (function() {
+  function ShitCray(crayDelay) {
+    if (crayDelay == null) {
+      crayDelay = 500;
+    }
+    this.shitCray = __bind(this.shitCray, this);
+    this.shitCray();
+    setInterval(this.shitCray, crayDelay);
+  }
+  ShitCray.prototype.LIKE_REPLACE = "Dat Shit Cray";
+  ShitCray.prototype.UNLIKE_REPLACE = "Not So Cray";
+  ShitCray.prototype.LIKE_THIS_REPLACE = "think this shit is cray";
+  ShitCray.prototype.LIKES_THIS_REPLACE = "thinks this shit is cray";
+  ShitCray.prototype.LIKE_TEXT_SELECTOR = [".uiUfiLike .UIImageBlock_Content"];
+  ShitCray.prototype.LIKE_BUTTON_SELECTOR = ["button.like_link .default_message", "button.cmnt_like_link .default_message", "a.likeButton .uiButtonText", "a.buttonLink .like"];
+  ShitCray.prototype.SAVING_SELECTOR = ["button.like_link .saving_message", "button.cmnt_like_link .saving_message", "a.buttonLink .unlike"];
+  ShitCray.prototype.replaceLike = function(elm, type) {
+    return elm.textContent = type === "like" ? this.LIKE_REPLACE : this.UNLIKE_REPLACE;
+  };
+  ShitCray.prototype.replaceLikeThis = function(elm) {
+    elm.textContent = elm.textContent.replace("like this", this.LIKE_THIS_REPLACE);
+    return elm.textContent = elm.textContent.replace("likes this", this.LIKES_THIS_REPLACE);
+  };
+  ShitCray.prototype.shitCray = function() {
+    var elm, likeElms, likeThisElms, type, _i, _j, _len, _len2, _ref;
+    likeElms = $(this.LIKE_BUTTON_SELECTOR + this.SAVING_SELECTOR);
+    likeThisElms = $(this.LIKE_TEXT_SELECTOR.join());
+    for (_i = 0, _len = likeElms.length; _i < _len; _i++) {
+      elm = likeElms[_i];
+      if (/like/i.test(type = elm.textContent.toLowerCase())) {
+        this.replaceLike(elm, type);
+      }
+    }
+    _ref = likeThisElms.contents();
+    for (_j = 0, _len2 = _ref.length; _j < _len2; _j++) {
+      elm = _ref[_j];
+      if (elm.nodeType === 3) {
+        this.replaceLikeThis(elm);
+      }
+    }
+  };
+  return ShitCray;
+})();
 $(function() {
-  
-  /*
-   * Selectors
-   */
-  var LIKE_BUTTON_SELECTOR = 'button.like_link .default_message, button.cmnt_like_link .default_message';
-  var SAVING_SELECTOR = 'button.like_link .saving_message, button.cmnt_like_link .saving_message';
-  var LIKE_TEXT_SELECTOR = '.uiUfiLike';
-  
-  /*
-   * Replacement strings
-   */
-  var LIKE_REPLACE = 'Dat Shit Cray';
-  var UNLIKE_REPLACE = 'Not So Cray';
-  var LIKE_THIS_REPLACE = 'think this shit is cray';
-  var LIKES_THIS_REPLACE = 'thinks this shit is cray';
-  
-  /*
-   * Do periodic replacements, since "Like" and "Unlike"
-   * are updated with AJAX
-   */
-  setInterval(function() {
-    // Replace "Like" and "Unlike"
-    $(LIKE_BUTTON_SELECTOR).each(function() {
-      replaceLike($(this));
-    });
-    $(SAVING_SELECTOR).each(function() {
-      replaceLike($(this));
-    });
-    // Replace "like this" and "likes this"
-    $(LIKE_TEXT_SELECTOR).each(function() {
-      replaceLikeThis($(this));
-    });
-  }, 200);
-  
-  /*
-   * Replace "Like" and "Unlike" text in given element
-   */
-  function replaceLike($elem) {
-    var original = $elem.text().toLowerCase();
-    if (original === "like") {
-      $elem.text(LIKE_REPLACE);
-    }
-    else if (original === "unlike") {
-      $elem.text(UNLIKE_REPLACE);
-    }
-  }
-  
-  /*
-   * Replace "like this" and "likes this" descriptive text,
-   * contained in a text node within this element
-   */
-  function replaceLikeThis($elem) {
-    $elem.contents().each(function() {
-      var _this = this;
-      if (_this.nodeType === 3) {
-        _this.textContent = _this.textContent.replace("like this", LIKE_THIS_REPLACE);
-        _this.textContent = _this.textContent.replace("likes this", LIKES_THIS_REPLACE);
-      }
-      else {
-        replaceLikeThis($(this));
-      }
-    });
-  }
-  
+  return new ShitCray(200);
 });
---
+++ src/craybook.coffee
@@ -0,0 +1,59 @@
+"use strict"
+
+class ShitCray
+    
+    # Dat shit about to get cray
+    constructor: (crayDelay = 500) ->
+        # Do the replacement functionality
+        do @shitCray
+    
+        # Do periodic replacements, since "Like" and "Unlike" 
+        # are updated with AJAX
+        setInterval @shitCray, crayDelay
+
+    # Replacement strings
+    LIKE_REPLACE: "Dat Shit Cray"
+    UNLIKE_REPLACE: "Not So Cray"
+    LIKE_THIS_REPLACE: "think this shit is cray"
+    LIKES_THIS_REPLACE: "thinks this shit is cray"
+  
+    # Selectors
+    LIKE_TEXT_SELECTOR: [
+        ".uiUfiLike .UIImageBlock_Content"                  # Comment box top
+    ]
+    LIKE_BUTTON_SELECTOR: [
+        "button.like_link .default_message"                 # Item like
+        "button.cmnt_like_link .default_message"            # Comment like
+        "a.likeButton .uiButtonText"                        # Picture like
+        "a.buttonLink .like"                                # Theatre picture like
+    ]
+    SAVING_SELECTOR: [
+        "button.like_link .saving_message"                  # Comment like  
+        "button.cmnt_like_link .saving_message"             # Picture like
+        "a.buttonLink .unlike"                              # Theatre picture like
+    ]
+ 
+    # Replace "Like" and "Unlike" text in given element
+    replaceLike: (elm, type) ->
+        elm.textContent = if type is "like" then @LIKE_REPLACE else @UNLIKE_REPLACE
+
+    # Replace "like this" and "likes this" descriptive text,
+    # contained in a text node within this element
+    replaceLikeThis: (elm) ->
+        elm.textContent = elm.textContent.replace "like this", @LIKE_THIS_REPLACE
+        elm.textContent = elm.textContent.replace "likes this", @LIKES_THIS_REPLACE
+        
+    # Do the replacement functionality
+    shitCray: =>
+        likeElms = $ @LIKE_BUTTON_SELECTOR + @SAVING_SELECTOR
+        likeThisElms = $ @LIKE_TEXT_SELECTOR.join()
+        
+        # Replace "Like" and "Unlike"
+        @replaceLike elm, type for elm in likeElms when /like/i.test(type = elm.textContent.toLowerCase())
+    
+        # Replace "like this" and "likes this"
+        @replaceLikeThis elm for elm in likeThisElms.contents() when elm.nodeType is 3
+    
+        return
+        
+$ -> new ShitCray 200
